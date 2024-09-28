1ba675432289d4e9ff386b94b91677b72a5b031b
PiyushBose <piyushbose05@gmail.com>
2024-09-28 18:54:44 +0530

    Gallery button functionality added

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function openGallery(){
-    
+    document.querySelector(".gallery-modal").classList.remove("hide");
+    document.querySelector("header").classList.add("blur");
+    document.querySelector("main").classList.add("blur");
+}
+
+function closeGallery(){
+    document.querySelector(".gallery-modal").classList.add("hide");
+    document.querySelector("header").classList.remove("blur");
+    document.querySelector("main").classList.remove("blur");
 }
